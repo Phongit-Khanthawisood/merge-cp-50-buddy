1cba7939763b5167360335143e2a4e01446e18cc
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2024-08-14 10:18:25 +0700

    feat: add easter egg page

--- src/components/StudentIdForm.tsx
+++ src/components/StudentIdForm.tsx
@@ -64,7 +64,7 @@ export default function StudentIdForm({
         description:
           "Your Pre-register in Web Complete -> Don't Forget to Register in gg form",
       });
-      window.location.href = "https://forms.gle/CLopzkR7SjhjRWgBA";
+      // window.location.href = "https://forms.gle/CLopzkR7SjhjRWgBA";
       setAlreadyUpdate(true);
     } catch (error) {
       console.error(error);
